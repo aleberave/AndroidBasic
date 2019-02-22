2743c6f4752247146fe9d3b2cb6e95d0e616a8c7
aleberave <lionessa@km.ru>
2019-02-22 15:36:19 +1000

    Lesson2HW

--- app/src/main/java/com/example/lesson2hw/MainActivity.java
+++ app/src/main/java/com/example/lesson2hw/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,15 +18,20 @@ public class MainActivity extends AppCompatActivity {
         final EditText editText1 = findViewById(R.id.editText1);
         final TextView textView2 = findViewById(R.id.textView2);
         final TextView textView4 = findViewById(R.id.textView4);
+        final TextView textView6 = findViewById(R.id.textView6);
 
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView2.setText(String.valueOf(editText1.getText()));
-                editText1.setText("");
-                textView4.setText("20 градусов");
-
+                if (editText1.getText().length() == 0){
+                    Toast.makeText(getApplicationContext(), "Введите название города", Toast.LENGTH_LONG).show();
+                } else {
+                    textView2.setText(String.valueOf(editText1.getText()));
+                    editText1.setText("");
+                    textView4.setText("20 градусов");
+                    textView6.setText("без осадков");
+                }
 
             }
         });
