23bd1175c632afc13784a19766fc4d043edc7ecd
aleberave <lionessa@km.ru>
2019-02-25 12:23:45 +1000

    Lesson3HW

--- app/src/main/java/com/example/lesson3hw/MainActivity.java
+++ app/src/main/java/com/example/lesson3hw/MainActivity.java
@@ -90,4 +90,33 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+        @Override
+        protected void onStart () {
+            super.onStart();
+        }
+
+        @Override
+        protected void onStop () {
+            super.onStop();
+        }
+
+        @Override
+        protected void onDestroy () {
+            super.onDestroy();
+        }
+
+        @Override
+        protected void onPause () {
+            super.onPause();
+        }
+
+        @Override
+        protected void onRestart () {
+            super.onRestart();
+        }
+
+        @Override
+        protected void onResume () {
+            super.onResume();
+        }
     }
