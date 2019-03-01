317e18ce5497d6d37535e1e4eaef9f55170388ab
aleberave <lionessa@km.ru>
2019-03-01 16:50:56 +1000

    Lesson4HW

---
+++ app/src/main/java/com/example/lesson4hw/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.lesson4hw;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TEXT = "text";
+    public static final String BOOL_CBT = "bool_cbt";
+    public static final String BOOL_CBW = "bool_cbw";
+    public static final String BOOL_SAP = "bool_sap";
+    public static final int REQUEST_CODE = 10;
+
+    public static boolean SAP = false;
+    public static boolean CBT = false;
+    public static boolean CBW = false;
+
+    private Button button;
+    private CheckBox checkBoxTemperature;
+    private CheckBox checkBoxWind;
+    private Switch switchAtmospherePressure;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.search);
+        button.setOnClickListener(new StartSecondActivity(this));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            EditText num = findViewById(R.id.writeNameTown);
+            num.setText(data.getStringExtra(TEXT));
+            CBT = Boolean.parseBoolean(data.getStringExtra(BOOL_CBT));
+            CBW = Boolean.parseBoolean(data.getStringExtra(BOOL_CBW));
+            SAP = Boolean.parseBoolean(data.getStringExtra(BOOL_SAP));
+
+            checkBoxTemperature = findViewById(R.id.checkBoxTemperature);
+            checkBoxTemperature.setChecked(CBT);
+
+            checkBoxWind = findViewById(R.id.checkBoxWind);
+            checkBoxWind.setChecked(CBW);
+
+            switchAtmospherePressure = findViewById(R.id.switchAtmospherePressure);
+            switchAtmospherePressure.setChecked(SAP);
+        }
+    }
+
+    public void onClickBox(View view) {
+        boolean checkedBox = ((CheckBox) view).isChecked();
+        switch (view.getId()) {
+            case R.id.checkBoxTemperature:
+                if (checkedBox) {
+                    CBT = true;
+                } else {
+                    CBT = false;
+                }
+                break;
+            case R.id.checkBoxWind:
+                if (checkedBox) {
+                    CBW = true;
+                } else {
+                    CBW = false;
+                }
+                break;
+        }
+    }
+
+    public void onClickSwitch(View view) {
+        boolean checkedSwitch = ((Switch) view).isChecked();
+        switch (view.getId()) {
+            case R.id.switchAtmospherePressure:
+                if (checkedSwitch) {
+                    SAP = true;
+                } else {
+                    SAP = false;
+                }
+                break;
+        }
+    }
+
+        @Override
+        protected void onStart () {
+            super.onStart();
+        }
+
+        @Override
+        protected void onStop () {
+            super.onStop();
+        }
+
+        @Override
+        protected void onDestroy () {
+            super.onDestroy();
+        }
+
+        @Override
+        protected void onPause () {
+            super.onPause();
+        }
+
+        @Override
+        protected void onRestart () {
+            super.onRestart();
+        }
+
+        @Override
+        protected void onResume () {
+            super.onResume();
+        }
+    }
---
+++ app/src/main/java/com/example/lesson4hw/Parsel.java
@@ -0,0 +1,12 @@
+package com.example.lesson4hw;
+
+import java.io.Serializable;
+
+public class Parsel implements Serializable {
+
+    public String text;
+    public boolean checkBoxTemperature;
+    public boolean checkBoxWind;
+    public boolean switchAtmospherePressure;
+
+}
